b8232d29771ba630a906b66012a2ac1b95c76432
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-06-05 00:05:41 +0700

    asdsa

--- portfolio/src/components/PersonalComponent.jsx
+++ portfolio/src/components/PersonalComponent.jsx
@@ -6,22 +6,33 @@ function PersonalComponent() {
             <div className='title-personal text-center'>
                 ประวัติ
             </div>
-            <div className="row">
-                <div className="col-12 sub-text">
-                    การศึกษา
-                </div>
-                <div className="col-12">
+            <div className='px-2'>
+                <div className="row">
+                    <div className="col-12 sub-text">
+                        การศึกษา
+                    </div>
                     <div className="box-personal">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eaque facere maiores doloremque aliquid dolore corrupti error enim quisquam! Reiciendis qui assumenda nisi nam officia doloribus dignissimos deleniti possimus eaque?
+                        <strong>ระดับมัธยมศึกษา (2557 - 2563)</strong>
+                        <br />
+                        โรงเรียนห้วยเม็กวิทยาคม สายวิทย์-คณิต
+                        GPAX : 3.47
+                        <hr />
+                        <strong>  ระดับอุดมศึกษา (2563-2567)</strong>
+                        <br />
+                        มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี คณะวิทยาศาสตร์และเทคโนโลยี สาขาวิทยาการคอมพิวเตอร์
+                        GPAX : 3.56
                     </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12 sub-text">
-                    ประสบการทำงาน
-                </div>
-                <div className="box-personal">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eaque facere maiores doloremque aliquid dolore corrupti error enim quisquam! Reiciendis qui assumenda nisi nam officia doloribus dignissimos deleniti possimus eaque?
+                <div className="row mt-3">
+                    <div className="col-12 sub-text">
+                        ประสบการทำงาน
+                    </div>
+                    <div className="box-personal">
+                        <strong>กรกฎาคม-ตุลาคม (2566)
+                            บริษัท ไอบิสซิเนส คอร์ปอเรชั่น จำกัด</strong>
+                        <br />
+                        ตำแหน่ง PHP Developer ทำการพัฒนาเว็บแอปพลิเคชันตามที่ได้รับมอบหมายทั้งด้าน Front-end และ Back-end โดยใช้ Laravel (สหกิจศึกษา)
+                    </div>
                 </div>
             </div>
         </div>
